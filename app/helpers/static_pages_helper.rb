2cc369708a3c8c18957cf41835890e879fac8986
awmha <alex@moranhook.com>
2018-06-05 17:41:32 -0400

    Helpers for thumbnails and carousels

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -17,4 +17,16 @@ module StaticPagesHelper
     no_display = ["no_display", "contact", "main"]
     @projects.where.not(category: no_display)
   end
+
+  def get_thumbnail(project)
+    if project.project_pictures.find_by(position: 0).nil?
+      return project.project_pictures.first.picture.image
+    else
+      return project.project_pictures.find_by(position: 0).picture.image
+    end
+  end
+
+  def gallery_images(project)
+    project.project_pictures.where.not(position: 0)
+  end
 end
