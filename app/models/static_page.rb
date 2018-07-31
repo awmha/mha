3286dc8934812cfe6a6cf2d3d1f7a5a48cb8f414
awmha <alex@moranhook.com>
2018-07-31 15:17:24 -0400

    Add favicon to public template

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -2,5 +2,4 @@ class StaticPage < ApplicationRecord
   store :social_media, accessors: [ "facebook", "twitter" ]
   mount_uploader :logo, PictureUploader
   mount_uploader :favicon, PictureUploader
-
 end
